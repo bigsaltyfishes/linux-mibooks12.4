d79cd1fa2a610a6a6440ae592446a0f5c25f8e47
bigsaltyfishes <bigsaltyfishes@gmail.com>
2024-08-12 20:30:57 +0800

    firmware: qcom_scm: Add Xiaomi Book 12.4 to list of tested QSEECOM devices
    Signed-off-by: bigsaltyfishes <bigsaltyfishes@gmail.com>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -1648,6 +1648,7 @@ EXPORT_SYMBOL_GPL(qcom_scm_qseecom_app_send);
 static const struct of_device_id qcom_scm_qseecom_allowlist[] __maybe_unused = {
 	{ .compatible = "lenovo,flex-5g" },
 	{ .compatible = "lenovo,thinkpad-x13s", },
+	{ .compatible = "xiaomi,book-12.4", },
 	{ .compatible = "qcom,sc8180x-primus" },
 	{ }
 };
